5c2f06c8e92c4119cbd5b8ff04487c8658c2a476
Gabbosenpai <gabrielemoglie493.gm@gmail.com>
2025-04-26 21:06:09 +0200

    Turret Placement
    Can't deselect towers yet, needs fix

---
+++ Scenes/tower_b_panel.gd
@@ -0,0 +1,23 @@
+extends Panel
+
+@onready var tower = preload("res://Scenes/Turrets/turret_b.tscn")
+
+func _on_gui_input(event: InputEvent) -> void:
+	var tempTower = tower.instantiate()
+	if event is InputEventMouseButton and event.button_mask == 1:
+		#Left Click Down
+		add_child(tempTower)
+		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+	elif  event is InputEventMouseMotion and event.button_mask == 1:
+		#Left Click Drag Down
+		if get_child_count() > 1:
+			get_child(1).global_position = event.global_position
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
+		var map = get_tree().get_root().get_node("TestScene")
+		map.add_child(tempTower)
+		tempTower.global_position = event.global_position
+	else:
+			if get_child_count() > 1:
+				get_child(1).queue_free()
