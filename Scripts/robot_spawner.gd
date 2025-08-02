2bb082660e2a1fd378dc4d733ba7c530a1d2ac62
Gabbosenpai <gabrielemoglie493.gm@gmail.com>
2025-08-02 16:02:31 +0200

    Update robot_spawner.gd

--- Scripts/robot_spawner.gd
+++ Scripts/robot_spawner.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var robot_A = preload("res://Scenes/Robots/robot_a.tscn")
-@onready var robot_B = preload("res://Scenes/Robots/robot_B.tscn")
+@onready var robot_B = preload("res://Scenes/Robots/robot_b.tscn")
 @onready var timer = $Timer
 
 @export var robot_power = 10
@@ -12,7 +12,8 @@ func _on_timer_timeout() -> void:
 	add_child(tempRobot)
 	robot_power -= 1
 	if robot_power == 0:
-		timer.wait_time = 5
+		robot_power = 7
+		timer.wait_time +=1
 		var boss = robot_B.instantiate()
 		add_child(boss)
 		boss.global_position.y = 245.0
