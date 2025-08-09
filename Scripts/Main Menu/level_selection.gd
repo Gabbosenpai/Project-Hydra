8affb4b08fb30390fd74de16d387bf6fb982e812
Andrew4993 <and.perinu@stud.uniroma3.it>
2025-08-09 15:38:57 +0200

    aggiunta musiche, modifica mappa, pulsanti indietro e fix bug muto

--- Scripts/Main Menu/level_selection.gd
+++ Scripts/Main Menu/level_selection.gd
@@ -5,5 +5,25 @@ extends Control
 func _on_level_1_pressed() -> void:
 	var level_music = preload("res://Assets/Sound/OST/The Whole Other - 8-Bit Dreamscape NO COPYRIGHT 8-bit Music( PRIMA WAVE LEVEL).mp3")
 	AudioManager.play_music(level_music)
-	
 	get_tree().change_scene_to_file("res://Scenes/Lvl1.tscn")
+
+#musica da scegliere
+#func _on_level_2_pressed() -> void:
+#	var level_music = preload("res://Assets/Sound/OST/.mp3")
+#	AudioManager.play_music(level_music)
+
+func _on_level_3_pressed() -> void:
+	var level_music = preload("res://Assets/Sound/OST/8-Bit Boss Battle： 4 - By EliteFerrex (ADVANCED WAVE LEVEL).mp3")
+	AudioManager.play_music(level_music)
+
+func _on_level_4_pressed() -> void:
+	var level_music = preload("res://Assets/Sound/OST/8Bit Boss Chiptune - Boss Theme - Sawsquarenoise · Free Copyright-Safe Music (WAVE OST).mp3")
+	AudioManager.play_music(level_music)
+
+func _on_level_5_pressed() -> void:
+	var level_music = preload("res://Assets/Sound/OST/8 Bit⧸16 Bit Music  - ＂This Is Our Only Chance!＂(FINAL LEVEL).mp3")
+	AudioManager.play_music(level_music)
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
