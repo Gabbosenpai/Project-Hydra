be1dfe9737cd3ca24bc3c1f5a72b13e876f1eb0c
Andrew4993 <and.perinu@stud.uniroma3.it>
2025-08-18 18:18:34 +0200

    backup pre inserimento save_data

--- Scripts/Main Menu/level_selection.gd
+++ Scripts/Main Menu/level_selection.gd
@@ -2,12 +2,29 @@ extends Control
 
 #tutto da aggiornare con gli altri 4 livelli
 
-func _ready():
-	var selection_music = preload("res://Assets/Sound/OST/Quincas Moreira - Robot City ♫ NO COPYRIGHT 8-bit Music (MENU AUDIO).mp3")
+#massimo livello sbloccato 
+var max_unlocked_level: int = 1
 
-	# Suona solo se è diversa da quella attualmente in riproduzione
+#prendiamo i bottoni dei livelli
+@onready var level_1_button = $Level1
+@onready var level_2_button = $Level2
+@onready var level_3_button = $Level3
+@onready var level_4_button = $Level4
+@onready var level_5_button = $Level5
+
+func _ready():
+	# Disattiva bottone se il max unlocked è minore del numero del livello
+	
+	level_2_button.disabled = max_unlocked_level < 2
+	level_3_button.disabled = max_unlocked_level < 3
+	level_4_button.disabled = max_unlocked_level < 4
+	level_5_button.disabled = max_unlocked_level < 5
+
+	# Suona la musica di selezione solo se diversa
+	var selection_music = preload("res://Assets/Sound/OST/Quincas Moreira - Robot City ♫ NO COPYRIGHT 8-bit Music (MENU AUDIO).mp3")
 	if AudioManager.music_player.stream != selection_music:
 		AudioManager.play_music(selection_music)
+
 
 func _on_level_1_pressed() -> void:
 	AudioManager.play_sfx(AudioManager.button_click_sfx)
