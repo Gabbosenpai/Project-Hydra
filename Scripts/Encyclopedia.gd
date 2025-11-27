a8f0e161b0c4373a573193c65de319e2371a8489
Andrew4993 <and.perinu@stud.uniroma3.it>
2025-11-27 18:16:26 +0100

    Bottoni elenchi cliccabili

--- Scripts/Encyclopedia.gd
+++ Scripts/Encyclopedia.gd
@@ -98,3 +98,15 @@ func ready():
 func _on_back_to_menu_pressed() -> void:
 	AudioManager.play_sfx(AudioManager.button_click_sfx)
 	get_tree().change_scene_to_file("res://Scenes/Utilities/menu.tscn")
+
+func _on_tower_button_pressed() -> void:
+	AudioManager.play_sfx(AudioManager.button_click_sfx)
+	get_tree().change_scene_to_file("res://Scenes/Utilities/menu.tscn")
+
+
+
+
+
+func _on_robot_button_pressed() -> void:
+	AudioManager.play_sfx(AudioManager.button_click_sfx)
+	get_tree().change_scene_to_file("res://Scenes/Utilities/menu.tscn")
