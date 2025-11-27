13459308466bfa635aa0d0410d48a9a0443f4337
Andrew4993 <and.perinu@stud.uniroma3.it>
2025-11-27 18:52:56 +0100

    Inizio schermata torrette

--- Scripts/EncyclopediaFirstScreen.gd
+++ Scripts/EncyclopediaFirstScreen.gd
@@ -101,7 +101,7 @@ func _on_back_to_menu_pressed() -> void:
 
 func _on_tower_button_pressed() -> void:
 	AudioManager.play_sfx(AudioManager.button_click_sfx)
-	get_tree().change_scene_to_file("res://Scenes/Utilities/menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/EncyclopediaTowerScreen.tscn")
 
 
 
