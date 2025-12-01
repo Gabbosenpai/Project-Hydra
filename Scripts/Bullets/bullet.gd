09f1d045df5afbb4400f8353c899a020f9d99644
Gabbosenpai <gabrielemoglie493.gm@gmail.com>
2025-12-01 17:24:21 +0100

    Bullet Script Convention

--- Scripts/Bullets/bullet.gd
+++ Scripts/Bullets/bullet.gd
@@ -2,15 +2,16 @@
 class_name Bullet
 extends Area2D
 
-# Nodi-Figlio della scena, inizializzati con onready perchè astratta
-@onready var bullet_sprite : AnimatedSprite2D = $BulletSprite
-@onready var bullet_hitbox : CollisionShape2D = $BulletHitbox
-@onready var vis_on_screen_not : VisibleOnScreenNotifier2D = $VisNot 
-
 # Variabili di un robot standard
-var speed : float
-var damage : int
-var hit : bool
+var speed: float
+var damage: int
+var hit: bool
+
+# Nodi-Figlio della scena, inizializzati con onready perchè astratta
+@onready var bullet_sprite: AnimatedSprite2D = $BulletSprite
+@onready var bullet_hitbox: CollisionShape2D = $BulletHitbox
+@onready var vis_on_screen_not: VisibleOnScreenNotifier2D = $VisNot 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,9 +22,11 @@ func _ready() -> void:
 	self.area_entered.connect(_on_area_entered)
 	bullet_sprite.animation_finished.connect(_on_bullet_sprite_animation_finished)
 
-func bullet_set_up(bullet_speed : float, bullet_damage : int) -> void:
+
+func bullet_set_up(bullet_speed: float, bullet_damage: int) -> void:
 	speed = bullet_speed
 	damage = bullet_damage
+
 
 # Funzione chiamata ad ogni frame fisico (numero fisso di frame al secondo)
 func _physics_process(delta):
@@ -35,10 +38,12 @@ func _physics_process(delta):
 		# Aggiorna la posizione globale del proiettile spostandolo in avanti
 		global_position += movement
 
+
 # Quando il nodo esce dallo schermo, deallocalo
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	# Distrugge il nodo, liberando la memoria e rimuovendo il proiettile
 	queue_free()
+
 
 # Controlla se il proiettile è entrato nell'area del nemico
 func _on_area_entered(area: Area2D):
@@ -48,6 +53,7 @@ func _on_area_entered(area: Area2D):
 		hit = true
 		enemy_node.take_damage(damage) #Se nell'area fa danno al nemico
 		bullet_sprite.play("explosion")
+
 
 func _on_bullet_sprite_animation_finished() -> void:
 	var current_animation = bullet_sprite.animation
