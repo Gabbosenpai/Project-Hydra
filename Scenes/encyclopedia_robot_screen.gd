8ee19d4ee9db9a3cb93d524456b244b863983d10
Andrew4993 <and.perinu@stud.uniroma3.it>
2025-12-01 18:25:34 +0100

    aggiunti robot e torrette mancanti (per ora)

--- Scenes/encyclopedia_robot_screen.gd
+++ Scenes/encyclopedia_robot_screen.gd
@@ -8,8 +8,11 @@ extends Control
 #da modificare con i percorsi giusti
 var monster_textures = {
 	"weed_eater": preload("res://Assets/Sprites/Robots/Weed Eater 9000/Weed Eater 9000.png"),
-	
-}
+	"mecha_freezer": preload("res://Assets/Sprites/Robots/Mecha Freezer/Mecha Freezer.png"),
+	"fire_hydrant": preload("res://Assets/Sprites/Robots/Fire Hydrant/Firehydrant.png"),
+	"romba": preload("res://Assets/Sprites/Robots/Romba/Romba.png"),
+	"cassa_schierata": preload("res://Assets/Sprites/Robots/Cassa Schierata/Cassa Schierata.png")
+}	
 
 
 var monster_names = {
@@ -24,8 +27,13 @@ var monster_names = {
 var monster_texts = {
 	
 	"weed_eater": "Questo è il Weed Eater! "+
-	"due ruote motrici, tre lame che fanno ognuna 800 rpm "+
+	"Due ruote motrici, tre lame che fanno ognuna 800 rpm "+
 	"e sembrano anche dei bei baffoni utili per tosare l'erba con stile!",
+	
+	"mecha_freezer": "Si dice che appaia nei centri abitati nelle notti in cui imperversano bufere di neve. Se bussa alla porta, non bisogna aprire per nessun motivo.",
+	"cassa_schierata": "Le sue urla si sentono a 10 km di distanza. Emette diversi suoni dalle numerose aperture sul suo corpo.",
+	"romba": "Ha gli arti superiori dotati di foglie affilate come rasoi e si muove agilmente fra i rami degli alberi per attaccare dall'alto i nemici.",
+	"fire_hydrant": "I cannoni sul suo guscio sparano getti d'acqua capaci di bucare l'acciaio."
 	
 }
 
@@ -76,3 +84,19 @@ func _on_back_pressed() -> void:
 
 func _on_weed_eater_pressed() -> void:
 	show_description("weed_eater")
+
+
+func _on_mecha_freezer_pressed() -> void:
+	show_description("mecha_freezer")
+
+
+func _on_cassa_schierata_pressed() -> void:
+	show_description("cassa_schierata")
+
+
+func _on_fire_hydrant_pressed() -> void:
+	show_description("fire_hydrant")
+
+
+func _on_romba_pressed() -> void:
+	show_description("romba")
