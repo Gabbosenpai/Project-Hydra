76cb07ae7ef119c3e3760eb34dd68c20b1022c77
Gabbosenpai <gabrielemoglie493.gm@gmail.com>
2025-12-09 18:49:27 +0100

    Missile Logic

--- Scripts/Bullets/sturamissile.gd
+++ Scripts/Bullets/sturamissile.gd
@@ -1,19 +1,23 @@
 class_name Sturamissile
 extends Bullet
 
-@export var sturamissile_speed: float = 200 # Velocità del proiettile
-@export var sturamissile_damage: int = 10 # Danno del proiettile
+@export var sturamissile_speed: float = 350 # Velocità del proiettile
+@export var sturamissile_damage: int = 42 # Danno del proiettile
 
 var armed: bool
+var explosion_range: Array
+
+@onready var explosion_blast: Area2D = $ExplosionBlast
+
 
 func _ready() -> void:
 	super.bullet_set_up(sturamissile_speed, sturamissile_damage)
 	super._ready()
+	bullet_hitbox.disabled = true
+	explosion_blast.area_entered.connect(_on_explosion_blast_area_entered)
 	armed = false
-	#var current_height = get
 	var tween1 = create_tween()
-	tween1.parallel().tween_property(self, "rotation_degrees", 90, 0.4)
-	#tween.parallel().tween_property(self, "position:y", global_position.y + 15, 0.5)
+	tween1.parallel().tween_property(self, "rotation_degrees", 90, 0.5)
 	var timer = get_tree().create_timer(0.5)
 	await timer.timeout
 	tween1.kill()
@@ -21,16 +25,42 @@ func _ready() -> void:
 	tween2.tween_property(self, "rotation_degrees", 0, 0.0)
 	bullet_sprite.play("travel")
 	armed = true
+	bullet_hitbox.disabled = false
 
 
 # Override
 # Funzione chiamata ad ogni frame fisico (numero fisso di frame al secondo)
 func _physics_process(delta):
 	if(!armed):
-		var jump = Vector2.UP * 70.0 * delta
+		var jump = Vector2.UP * 80.0 * delta
 		global_position += jump
 	if(!hit and armed):
-		var movement = Vector2(+1, 5) * Vector2(speed, 1) * delta
+		var movement = Vector2(1,1) * Vector2(speed, 5) * delta
 		bullet_sprite.play("travel")
 		# Aggiorna la posizione globale del proiettile spostandolo in avanti
 		global_position += movement
+
+
+func explode() -> void:
+	for robot in explosion_range:
+		if (
+				robot != null 
+				and is_instance_valid(robot) 
+				and robot.has_method("take_damage")
+		):
+			robot.take_damage(damage)
+
+
+# Override
+# Controlla se il proiettile è entrato nell'area del nemico
+func _on_area_entered(robot: Area2D):
+	if robot.is_in_group("Robot"):
+		if(!hit):
+			hit = true
+			explode()
+			bullet_sprite.play("explosion")
+
+
+func _on_explosion_blast_area_entered(robot: Area2D):
+	if robot.is_in_group("Robot"):
+		explosion_range.append(robot)
