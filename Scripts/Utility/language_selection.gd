da7f1c935319d79fff0966fcc07e227d90a47193
Andrew4993 <and.perinu@stud.uniroma3.it>
2026-01-10 16:40:41 +0100

    bottoni lingua mutualmente esclusivi

--- Scripts/Utility/language_selection.gd
+++ Scripts/Utility/language_selection.gd
@@ -2,21 +2,36 @@ extends Control
 
 @onready var italian_button = $ItalianoButton
 @onready var english_button = $EnglishButton
-
-
+@onready var chinese_button = $ChineseButton
 func _ready():
-	# lingua predefinita 
+	# lingua predefinita
 	TranslationServer.set_locale("it")
-
+	italian_button.set_pressed(true)  
 
 func _on_italiano_button_pressed():
+	
+	italian_button.set_pressed(true)
+	english_button.set_pressed(false)
+	chinese_button.set_pressed(false)
 	AudioManager.play_sfx(AudioManager.button_click_sfx)
 	TranslationServer.set_locale("it")
 
-
 func _on_english_button_pressed():
+	
+	english_button.set_pressed(true)
+	italian_button.set_pressed(false)
+	chinese_button.set_pressed(false)
 	AudioManager.play_sfx(AudioManager.button_click_sfx)
 	TranslationServer.set_locale("en")
+
+
+func _on_chinese_button_pressed() -> void:
+	
+	chinese_button.set_pressed(true)
+	italian_button.set_pressed(false)
+	english_button.set_pressed(false)
+	AudioManager.play_sfx(AudioManager.button_click_sfx)
+	TranslationServer.set_locale("zh")
 
 
 func _on_back_to_menu_pressed() -> void:
