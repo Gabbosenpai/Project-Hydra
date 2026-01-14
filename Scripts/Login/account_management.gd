f654b7ee78e292ec903d8cf76a86c7bf7b29a246
Gabbosenpai <gabrielemoglie493.gm@gmail.com>
2026-01-14 16:20:35 +0100

    Username Centered

--- Scripts/Login/account_management.gd
+++ Scripts/Login/account_management.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var status_label = $CenterContainer/UserLabel
+@onready var status_label = $UserLabel
 @onready var confirmation_dialog = $ConfirmationDialog
 
 func _ready():
