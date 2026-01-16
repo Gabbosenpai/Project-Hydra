84d023ec9fecea0589e3188c3437680b566f3e60
Gabbosenpai <gabrielemoglie493.gm@gmail.com>
2026-01-16 17:00:15 +0100

    Better Level Selection

--- Scripts/Main Menu/level_selection.gd
+++ Scripts/Main Menu/level_selection.gd
@@ -6,7 +6,11 @@ extends Control
 @onready var level_3: TextureButton = $Level3
 @onready var level_4: TextureButton = $Level4
 @onready var level_5: TextureButton = $Level5
+@onready var blink_timer: Timer = $BlinkTimer
 
+var blink: bool = false
+var lvl_done = preload("res://Assets/Sprites/UI/Menu/Small Confirm Button Pressed.png")
+var lvl_blocked = preload("res://Assets/Sprites/UI/Menu/Small Confirm Button Not Pressed.png")
 
 
 func _ready():
@@ -24,18 +28,85 @@ func _ready():
 	refresh_select_lvl_ui(max_unlocked_level)
 
 
+func check_lvl():
+	var max_lvl: int = SaveManager.get_max_unlocked_level()
+	match max_lvl:
+		1:
+			if blink:
+				level_1.texture_normal = lvl_blocked
+				blink = false
+			else:
+				level_1.texture_normal = lvl_done
+				blink = true
+		2:
+			if blink:
+				level_2.texture_normal = lvl_blocked
+				blink = false
+			else:
+				level_2.texture_normal = lvl_done
+				blink = true
+		3:
+			if blink:
+				level_3.texture_normal = lvl_blocked
+				blink = false
+			else:
+				level_3.texture_normal = lvl_done
+				blink = true
+		4:
+			if blink:
+				level_4.texture_normal = lvl_blocked
+				blink = false
+			else:
+				level_4.texture_normal = lvl_done
+				blink = true
+		5:
+			if blink:
+				level_5.texture_normal = lvl_blocked
+				blink = false
+			else:
+				level_5.texture_normal = lvl_done
+				blink = true
+
+
 func refresh_select_lvl_ui(max_lvl):
 	match max_lvl:
 		1:
 			pass
 		2:
-			pass
+			level_1.texture_normal = lvl_done
+			level_1.texture_pressed = lvl_blocked
 		3:
-			pass
+			level_1.texture_normal = lvl_done
+			level_1.texture_pressed = lvl_blocked
+			level_2.texture_normal = lvl_done
+			level_2.texture_pressed = lvl_blocked
 		4:
-			pass
+			level_1.texture_normal = lvl_done
+			level_1.texture_pressed = lvl_blocked
+			level_2.texture_normal = lvl_done
+			level_2.texture_pressed = lvl_blocked
+			level_3.texture_normal = lvl_done
+			level_3.texture_pressed = lvl_blocked
 		5:
-			pass
+			level_1.texture_normal = lvl_done
+			level_1.texture_pressed = lvl_blocked
+			level_2.texture_normal = lvl_done
+			level_2.texture_pressed = lvl_blocked
+			level_3.texture_normal = lvl_done
+			level_3.texture_pressed = lvl_blocked
+			level_4.texture_normal = lvl_done
+			level_4.texture_pressed = lvl_blocked
+		6:
+			level_1.texture_normal = lvl_done
+			level_1.texture_pressed = lvl_blocked
+			level_2.texture_normal = lvl_done
+			level_2.texture_pressed = lvl_blocked
+			level_3.texture_normal = lvl_done
+			level_3.texture_pressed = lvl_blocked
+			level_4.texture_normal = lvl_done
+			level_4.texture_pressed = lvl_blocked
+			level_5.texture_normal = lvl_done
+			level_5.texture_pressed = lvl_blocked
 
 
 func _on_level_1_pressed() -> void:
@@ -84,3 +155,7 @@ func _on_level_5_pressed() -> void:
 func _on_back_button_pressed() -> void:
 	AudioManager.play_sfx(AudioManager.button_click_sfx)
 	get_tree().change_scene_to_file("res://Scenes/Utilities/slot_selection.tscn")
+
+
+func _on_blink_timer_timeout() -> void:
+	check_lvl()
