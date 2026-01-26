2f05e728a60c91142804539f7940bcae5db34f2b
Gabbosenpai <gabrielemoglie493.gm@gmail.com>
2026-01-27 00:52:25 +0100

    better cutscene

--- Scenes/Utilities/cutscene.gd
+++ Scenes/Utilities/cutscene.gd
@@ -1,5 +1,22 @@
 extends Node
 
+var concluso: bool = false
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	await animation_player.animation_finished
+	concluso = true
 
 func go_to_menu():
-	get_tree().change_scene_to_file("res://Scenes/Utilities/menu.tscn")
+	pass
+	#get_tree().change_scene_to_file("res://Scenes/Utilities/")
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		#if not concluso:
+		salta_intro()
+
+func salta_intro():
+	animation_player.advance(10.0)
+	#if animation_player.
