d2fa8601f0c85eb0a9590407e50a7eb50dcf5b08
Andrew4993 <and.perinu@stud.uniroma3.it>
2026-01-29 17:35:02 +0100

    frasi mail e tutti i crediti (sigh)

--- Scripts/Main Menu/credits.gd
+++ Scripts/Main Menu/credits.gd
@@ -6,30 +6,33 @@ extends Control
 func _ready():
 	var credits_music = preload("res://Assets/Sound/OST/Kevin MacLeod - Itty Bitty (CREDITS THEME).mp3")
 
-	var credits_text := "OST" + "\n"
+#CREATED BY ANDREA,GABRILE MOGLIÉ,MICHELE
+#GAME DESIGN ANDREA,GABRIELE,MICHELE
+#PROGRAMMING ANDREA,GABRIELE,MICHELE
+#2D ART & ANIMATION GABRIELE 
+#SOUND DESIGN ANDREA
+#UI DESIGN GABRIELE
+#WRITING GABRIELE
+#PLAYTESTING ANDREA, MICHELE
+#LOCALIZATION ANDREA
+#OST CI SONO GIA
+
+
+	var credits_text := tr("created_by") + "\n" + "Andrea Perinu\nGabriele Moglié\nMichele Carta\n\n" 
+	credits_text+=tr("game_design") + "\n" + "Andrea Perinu\nGabriele Moglié\nMichele Carta\n\n"
+	credits_text+=tr("programming") + "\n" + "Andrea Perinu\nGabriele Moglié\nMichele Carta\n\n"
+	credits_text+=tr("2d_art") + "\n" + "Gabriele Moglié\n\n" 
+	credits_text+=tr("sound_design") + "\n" + "Andrea Perinu\n\n"
+	credits_text+=tr("UI_design") + "\n" + "Gabriele Moglié\n\n"
+	credits_text+=tr("writing") + "\n" + "Gabriele Moglié\n\n"
+	credits_text+=tr("playtesting") + "\n" + "Andrea Perinu\nMichele Carta\n\n"
+	credits_text+=tr("localization") + "\n" + "Andrea Perinu\n\n"
+	credits_text += "OST" + "\n" 
 	credits_text += "Quincas Moreira – Robot City\nCody O'Quinn – Scrub Slayer\n"
 	credits_text += "Epic Music Journey – 8 BIT RPG BATTLE\nAdventureChiptunes – NEW POWER\n"
 	credits_text += "HeatleyBros – 8 Bit Scrap!\nHeatleyBros – 8 BIT BOSS\n"
 	credits_text += "Pix – A Lonely Cherry Tree\nAdam Haynes – 8-bit Victory Theme\nKevin MacLeod – Itty Bitty\n"
 	credits_text += "Party's Cancelled - RoccoW ｜ Chiptune\n\n"
-	
-	credits_text += tr("UI_design")+"\n"
-	credits_text += "da completare\n\n"
-	
-	credits_text += tr("monster_design")+"\n"
-	credits_text += "da completare\n\n"
-	
-	credits_text += tr("programming")+"\n"
-	credits_text += "da completare\n\n"
-	
-	credits_text += tr("balancing_testing")+"\n"
-	credits_text += "da completare\n\n"
-	
-	credits_text += tr("localization")+"\n"
-	credits_text += "da completare\n\n"
-	
-	
-	
 	
 	$Label.text = credits_text
 
